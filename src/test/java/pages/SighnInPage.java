1ac3f0acf3c83143940083fbd9760790ea0838de
Gulzhana Malikova <gulzhanamalikova@Gulzhanas-MacBook-Pro.local>
2020-01-20 16:55:02 -0500

    Sign In Verification

--- src/test/java/pages/SighnInPage.java
+++ src/test/java/pages/SighnInPage.java
@@ -1,7 +1,10 @@
 package pages;
 
 import base.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utils.SeleniumUtils;
 
 public class SighnInPage extends TestBase {
 
@@ -11,6 +14,27 @@ public class SighnInPage extends TestBase {
 
     }
 
+    @FindBy(id="ods-input-0")
+    public WebElement userName;
 
+
+
+    @FindBy(id="ods-input-1")
+    public WebElement password;
+
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public  WebElement signInBtn;
+
+
+
+
+
+    public void signIn(){
+        SeleniumUtils.sendKeys(userName,"ABCD");
+        SeleniumUtils.sendKeys(password,"1234");
+        SeleniumUtils.click(signInBtn);
+
+    }
 
 }
--- src/test/java/tests/SighnInPageTest.java
+++ src/test/java/tests/SighnInPageTest.java
@@ -1,16 +1,47 @@
 package tests;
 
+import base.TestBase;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.BusinessPage;
+import pages.HomePage;
+import pages.SighnInPage;
 import utils.SeleniumUtils;
 
-public class SighnInPageTest {
+public class SighnInPageTest extends TestBase {
+
+   HomePage home;
+   BusinessPage business;
+   SighnInPage signIn;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(){
+        initializer();
+        home = new HomePage();
+        business= new BusinessPage();
+        signIn =new SighnInPage();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(){
+        driver.quit();
+    }
 
 
 
+    @Test
+    public void  virifySignIn(){
+        home.navigateToBusinessPage();
+        business.clickSighnIn();
+        signIn.signIn();
 
-//    @Test
-//    public  void sighnIn(){
-//        Assert.assertEquals(SeleniumUtils.click(homePage.sighnIn));
-//    }
+    }
+    @Test
+    public  void verifyTitle(){
+        Assert.assertEquals(driver.getTitle(),"Capital One Credit Cards, Bank, and Loans - Personal and Business");
+    }
+
+
 }
